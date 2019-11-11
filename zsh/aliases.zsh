f39e2630cb7b30be0846a10147633a97f1b77145
Tony Yates <tony@tonebook.local>
2019-11-11 14:37:41 +0000

    added k8s helpers

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,4 @@
 alias reload!='. ~/.zshrc'
 alias anacondainit='export PATH="/Users/tony/anaconda/bin:$PATH"'
 alias flushdns='dscacheutil -flushcache;sudo killall -HUP mDNSResponder'
+alias k8all='kubectl get po,deployments,svc,ing'
