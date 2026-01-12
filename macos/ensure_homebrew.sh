dd49e3559326e3d752beff3312acd37a42a3b030
Tony Yates <tony.yates@kry.se>
2026-01-12 17:27:15 +0000

    Restored scripts dir

--- macos/ensure_homebrew.sh
+++ macos/ensure_homebrew.sh
@@ -4,10 +4,10 @@
 # Check if Homebrew is installed
 #
 which -s brew
-if [[ $? != 0 ]] ; then
-	# Install Homebrew
-	# http://brew.sh/
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if [[ $? != 0 ]]; then
+  # Install Homebrew
+  # http://brew.sh/
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
-	brew update
+  brew update
 fi
